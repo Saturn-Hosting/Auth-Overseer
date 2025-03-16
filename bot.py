015308e8e12c374b23f353403c0e6d8bf456e506
Fulgur <fulgurgithub@gmail.com>
2025-03-16 14:54:27 +0100

    api

--- bot.py
+++ bot.py
@@ -2,16 +2,23 @@ from javascript import require, On
 import time
 import random
 import requests
+from flask import Flask, jsonify
+import threading
+
 mineflayer = require('mineflayer')
 
 allowedChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 
 webhook_url = "https://discord.com/api/webhooks/1350822392850546709/pyDfo-aarTvbzWs-rf09a8YRuv7_f7FIXcajAqDO85vj8mT9Rs1GmoZ08UuRurytZKop"
 
+app = Flask(__name__)
+current_accounts_in_auth = []
+
 def getUsername():
     return 'Apostle_' + ''.join(random.choices(allowedChars, k=7))
 
 def connect():
+    global current_accounts_in_auth
     try:
         username = getUsername()
         bot = mineflayer.createBot({
@@ -56,6 +63,7 @@ def connect():
                             requests.post(webhook_url, json={'content': f'❌ **{acc}** left auth'})
 
                 oldAccsInAuth = newAccsInAuth
+                current_accounts_in_auth = newAccsInAuth  # Update the global list
                 time.sleep(5)
 
         @On(bot, 'end')
@@ -75,5 +83,16 @@ def connect():
         print(e)
         time.sleep(5)
         connect()
+
+@app.route('/current-accounts', methods=['GET'])
+def get_current_accounts():
+    return jsonify(current_accounts_in_auth)
+
+def run_flask():
+    app.run(host='0.0.0.0', port=5069)
+
+flask_thread = threading.Thread(target=run_flask)
+flask_thread.daemon = True
+flask_thread.start()
 
 connect()
